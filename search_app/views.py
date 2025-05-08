bc1d9e86ae1f6c506dd6fa816038eb57343366e4
Muhammad Rafi Zia Ulhaq <mohrafiziaulhaq@gmail.com>
2025-05-08 21:51:11 +0700

    add search page

--- search_app/views.py
+++ search_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def search_view(request):
+    context = {}
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        context['query'] = query
+        context['answer'] = f"Result: '{query}'"
+    
+    return render(request, 'search.html', context)
